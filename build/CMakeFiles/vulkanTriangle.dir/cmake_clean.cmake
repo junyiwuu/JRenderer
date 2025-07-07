682716a116631d74943184a1b68a4e6100e987a7
j <j@jscomp.mynet>
2025-07-07 18:08:04 +0100

    starting point

---
+++ build/CMakeFiles/vulkanTriangle.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vulkanTriangle.dir/src/triangle.cpp.o"
+  "CMakeFiles/vulkanTriangle.dir/src/triangle.cpp.o.d"
+  "vulkanTriangle"
+  "vulkanTriangle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vulkanTriangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/vulkanTriangle.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vulkanTriangle.
+# This may be replaced when dependencies are built.
